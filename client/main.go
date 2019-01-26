8649fcd8307ce0ddf531d92b8cf9019ce40bd998
kaito2 <kaito.iwatsuki@nislab.doshisha.ac.jp>
2019-01-27 08:52:06 +0900

    stream server 実装

--- client/main.go
+++ client/main.go
@@ -1,1 +1,38 @@
 package main
+
+import (
+	"context"
+	"go-ocgrpc-stream-try/protos"
+	"google.golang.org/grpc"
+	"io"
+	"log"
+)
+
+const (
+	address = "localhost:50051"
+)
+
+func main() {
+	conn, err := grpc.Dial(address, grpc.WithInsecure())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	client := grpcstream.NewSampleStreamClient(conn)
+
+	req := grpcstream.SampleRequest{Message: "Request Message"}
+	stream, err := client.GetSampleResponse(context.Background(), &req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		feature, err := stream.Recv()
+		if err == io.EOF { // サーバ側でストリーミングが正常に終了(return nil)された
+			break
+		}
+		if err != nil {
+			log.Fatalf("%v.ListFeatures(_) = _, %v", client, err)
+		}
+		log.Println(feature)
+	}
+}
--- server/main.go
+++ server/main.go
@@ -1,1 +1,40 @@
 package main
+
+import (
+	pb "go-ocgrpc-stream-try/protos"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+	"time"
+)
+
+const (
+	listenAddress = "0.0.0.0:50051"
+)
+
+type MySampleStreamServer struct {}
+
+func (s *MySampleStreamServer) GetSampleResponse(req *pb.SampleRequest, stream pb.SampleStream_GetSampleResponseServer) error {
+	// 固定の文字列を返すだけ
+	res := pb.SampleResponse{Message: "Reply Message"}
+	for {
+		time.Sleep(1 * time.Second)
+		if err := stream.Send(&res); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", listenAddress)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterSampleStreamServer(grpcServer, &MySampleStreamServer{})
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %s", err)
+	}
+}
